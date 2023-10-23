1225aed7378b82c1ad5b2e0fe33c3e138f1f12e8
sseyeon <seyeon9809@gmail.com>
2023-10-23 17:42:12 +0900

    modify Product

---
+++ src/contexts/SidebarContext.js
@@ -0,0 +1,9 @@
+import React, {useState, createContext} from 'react';
+// create context
+export const SidebarContext = createContext();
+const SidebarProvider = ({children}) => {
+  return <SidebarContext.Provider>
+    {children}
+  </SidebarContext.Provider>;
+}
+export default SidebarProvider;
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+// import product context
+import { ProductContext } from '../contexts/ProductContext';
+
+// import components
+import Product from '../components/Product';
+
+const Home = () => {
+  // get products from product context
+  const { products } = useContext(ProductContext);
+  // console.log(products);
+  // get only men;s & women's clothing category
+  const filteredProducts = products.filter(item=>{
+    return item.category === "men's clothing" || item.category === "women's clothing" ;
+  })
+  return (
+  <div>
+    <section className='p-16'>
+      <div className='container mx-auto'>
+        <div className='gird grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none mx-0'>
+          {filteredProducts.map((product) => {
+            return (
+                <Product product={product} key={product.id}></Product>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  </div>);
+};
+
+export default Home;
