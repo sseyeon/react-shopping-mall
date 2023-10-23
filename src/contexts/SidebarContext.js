92edc1fb615806de73b96ac779fa285836380e5f
sseyeon <seyeon9809@gmail.com>
2023-10-23 17:50:31 +0900

    modify README

--- src/contexts/SidebarContext.js
+++ src/contexts/SidebarContext.js
@@ -2,6 +2,7 @@ import React, {useState, createContext} from 'react';
 // create context
 export const SidebarContext = createContext();
 const SidebarProvider = ({children}) => {
+  // sidebar state
   return <SidebarContext.Provider>
     {children}
   </SidebarContext.Provider>;
