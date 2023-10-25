0374112acecd51968f054bfbda4810604164d775
sseyeon <seyeon9809@gmail.com>
2023-10-26 00:14:56 +0900

    modify Home grid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,16 @@ import Hero from '../components/Hero';
 const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
-  // console.log(products);
   // get only men;s & women's clothing category
   const filteredProducts = products.filter(item=>{
     return item.category === "men's clothing" || item.category === "women's clothing" ;
-  })
+  });
   return (
   <div>
     <Hero />
     <section className='p-16'>
       <div className='container mx-auto'>
-        <div className='gird grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none mx-0'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none mx-0'>
           {filteredProducts.map((product) => {
             return (
                 <Product product={product} key={product.id}></Product>
